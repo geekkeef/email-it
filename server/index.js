cc1b9fcdd704800d5ec814a72ebaf09a7b290c53
geekkeef <keithtjunior@gmail.com>
2018-03-09 08:05:15 -0500

    changed greeting

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express'); // common js module = 'require'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ geek: 'Keef' });
 });
 
-const PORT = process.env.PORT ||  5000;
+const PORT = process.env.PORT || 5000;
 
 // express tells node to listen to a specific port
 app.listen(PORT);
